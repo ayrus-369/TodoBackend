771664f45be8003d25a83f6d0f7847205236b2cd
Surya <85205650+ayrus-369@users.noreply.github.com>
2023-05-27 00:39:36 +0530

    Uploaded the backend files

---
+++ Server.js
@@ -0,0 +1,13 @@
+ const express =require('express')
+ const mongoose =require ('mongoose')
+ const routes =require('./routes/ToDoRoute')
+ const cors=require("cors")
+ require('dotenv').config()
+ const app=express()
+ const PORT = process.env.PORT || 5000
+app.use(express.json())
+app.use(cors())
+ mongoose.connect(process.env.MONGODB_URL)
+ .then(()=>console.log("connected To mongodb...")).catch((err)=>console.log(err))
+ app.use(routes)
+ app.listen( PORT,()=> console.log(`Listening on : ${PORT}`))
